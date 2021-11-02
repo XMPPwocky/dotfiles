8dd9adbcb8af977f17e304cbc9d244d8c626ac3a
Mimir <mimir@xmppwocky.net>
2021-11-02 10:48:48 -0700

    try switching to pipewire

--- modules/audio.nix
+++ modules/audio.nix
@@ -1,31 +1,22 @@
 { pkgs, ... }:
 {
-  # build everything with pulseaudio where available
-  nixpkgs.config.pulseaudio = true;
-
-  environment.systemPackages = with pkgs; [ libjack2 jack2 qjackctl ];
-
   security.pam.loginLimits = [
     { domain = "@audio"; item = "memlock"; type = "*"; value = "unlimited"; }
     { domain = "@audio"; item = "rtprio"; type = "*"; value = "99"; }
     { domain = "@audio"; item = "nofile"; type = "soft"; value = "99999"; }
     { domain = "@audio"; item = "nofile"; type = "hard"; value = "99999"; }
   ];
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
 
-  hardware.pulseaudio.enable = true;
-  hardware.pulseaudio.package = pkgs.pulseaudio.override { jackaudioSupport = true; };
-
-  services.jack = {
-    jackd.enable = false;
-    # support ALSA only programs via ALSA JACK PCM plugin
-    alsa.enable = false;
-    # support ALSA only programs via loopback device (supports programs like Steam)
-    loopback = {
-      enable = true;
-      # buffering parameters for dmix device to work with ALSA only semi-professional sound programs
-      #dmixConfig = ''
-      #  period_size 2048
-      #'';
-    };
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
   };
 }
