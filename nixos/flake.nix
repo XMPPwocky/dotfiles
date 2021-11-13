531b05d779f3bbe5e0109adad4042c4dd95c256e
Mimir <mimir@xmppwocky.net>
2021-11-13 12:45:26 -0800

    try to module harder

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -6,19 +6,38 @@
     mimir-home.url = "path:/home/mimir/dotfiles/mimir-home";
   };
 
-  outputs = { self, home-manager, nixpkgs, mimir-home }: {
-    nixosConfigurations.mimir-nixos-fw = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./machines/mimir-nixos-fw/configuration.nix
+  outputs = { self, home-manager, nixpkgs, mimir-home }:
+    let
+      modules = {
+        hardening = modules/hardening.nix;
 
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.mimir = mimir-home.outputs.hmCfg;
-        }
-      ];
+        #modules/enable-flakes.nix
+
+        #modules/power-utils.nix
+        #modules/basic-users.nix
+        #modules/desktop.nix
+        #modules/audio.nix
+        #modules/ps5-controller-udev.nix
+        #modules/tailscale.nix
+      };
+    in
+    {
+      nixosModules = modules;
+
+      nixosConfigurations.mimir-nixos-fw = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./machines/mimir-nixos-fw/configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.mimir = mimir-home.outputs.hmCfg;
+          }
+
+	  modules.hardening
+        ];
+      };
     };
-  };
 }
